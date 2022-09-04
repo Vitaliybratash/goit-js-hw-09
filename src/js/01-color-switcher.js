df87fa1f760a481c2d3749edb4428a2de96e9efc
Vitaliy <vitaliy.bratash23@gmail.com>
2022-09-04 18:31:14 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,17 @@
+const start = document.querySelector('[data-start]');
+const stop = document.querySelector('[data-stop]');
+let interval = '';
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+start.addEventListener('click', () => {
+  interval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+  start.disabled = true;
+});
+stop.addEventListener('click', () => {
+  clearInterval(interval);
+  start.disabled = false;
+});
